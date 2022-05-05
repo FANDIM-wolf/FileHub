c9fcbba590ccd4d29bdab038ae50601c7ecce7fa
Mikhail Yolkin <63235817+FANDIM-wolf@users.noreply.github.com>
2022-05-05 16:34:25 +0300

    Add files via upload

--- token.h
+++ token.h
@@ -11,10 +11,23 @@ QString get_random_symbol_of_alphabet(){
     return alphabet[random_value];
 }
 
+QString get_random_symbol_of_number(){
+    QString alphabet_numbers[10] ={"0","1","2","3","4","5","6","7","8","9"};
+    int random_value = qrand()%10;
+    return alphabet_numbers[random_value];
+}
+
+int put_number_or_not(){
+    return qrand()%1;
+}
+
 QString get_random_token(){
 
     QString  token;
     for(int i=0 ; i<= 10 ;i++){
+        if(put_number_or_not() == 1){
+            token += get_random_symbol_of_number();
+        }
         token += get_random_symbol_of_alphabet();
     }
     return token;
