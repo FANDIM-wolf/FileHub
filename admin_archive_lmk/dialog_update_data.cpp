5321afdc942b559cd9a24dffe153496fb07d2a3f
Mikhail Shishov <63235817+FANDIM-wolf@users.noreply.github.com>
2022-05-22 10:52:47 +0300

    Add files via upload

--- admin_archive_lmk/dialog_update_data.cpp
+++ admin_archive_lmk/dialog_update_data.cpp
@@ -1,6 +1,9 @@
 #include "dialog_update_data.h"
 #include "ui_dialog_update_data.h"
 #include "ENV.h"
+
+QString type_of_data =" ";
+
 Dialog_update_data::Dialog_update_data(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Dialog_update_data)
@@ -19,6 +22,7 @@ Dialog_update_data::~Dialog_update_data()
 
 void Dialog_update_data::on_pushButton_clicked()
 {
+    type_of_data = "vistitors";
     QSqlDatabase conn = QSqlDatabase::addDatabase("QPSQL");
         conn.setHostName("127.0.0.1");
         conn.setUserName("postgres");
@@ -49,6 +53,9 @@ void Dialog_update_data::on_pushButton_clicked()
 void Dialog_update_data::on_tableView_clicked(const QModelIndex &index)
 {
 
+
+   if (type_of_data == "vistitors"){
+
     int selected_code = index.row()+CORRECTING_ROW;
        row = selected_code;
        qDebug()<<row;
@@ -58,6 +65,7 @@ void Dialog_update_data::on_tableView_clicked(const QModelIndex &index)
        db.setPassword("elkin");
        db.setDatabaseName("postgres");
        if(db.open()){
+
                query = new QSqlQuery(db);
                           //write down sql request
                query->prepare("SELECT * FROM vistitors WHERE id=? ");
@@ -79,6 +87,78 @@ void Dialog_update_data::on_tableView_clicked(const QModelIndex &index)
                            }
        }
        db.close();
+    }
+    if(type_of_data =="documents"){
+
+        int selected_code = index.row()+1;
+           row = selected_code;
+           qDebug()<<row;
+           QSqlDatabase db = QSqlDatabase::database();
+           db.setHostName("127.0.0.1");
+           db.setUserName("postgres");
+           db.setPassword("elkin");
+           db.setDatabaseName("postgres");
+           if(db.open()){
+
+                   query = new QSqlQuery(db);
+                              //write down sql request
+                   query->prepare("SELECT * FROM documents WHERE id=? ");
+                   query->bindValue(0,selected_code); // bind
+
+                   if(query->exec()){
+                               if(query->first()){
+
+                                  QString name_of_document =query->value(1).toString();
+                                  QString group_of_user = query->value(2).toString();
+
+                                  ui->lineEdit->setText(name_of_document);
+                                  ui->lineEdit_2->setText(group_of_user);
+
+
+
+                               }
+
+                               }
+           }
+           db.close();
+    }
+
+    if(type_of_data == "books"){
+        int selected_code = index.row()+1;
+           row = selected_code;
+           qDebug()<<row;
+           QSqlDatabase db = QSqlDatabase::database();
+           db.setHostName("127.0.0.1");
+           db.setUserName("postgres");
+           db.setPassword("elkin");
+           db.setDatabaseName("postgres");
+           if(db.open()){
+
+                   query = new QSqlQuery(db);
+                              //write down sql request
+                   query->prepare("SELECT * FROM books WHERE id=? ");
+                   query->bindValue(0,selected_code); // bind
+
+                   if(query->exec()){
+                               if(query->first()){
+
+                                  QString name_of_document =query->value(1).toString();
+                                  QString image = query->value(2).toString();
+                                  QString description = query->value(3).toString();
+                                  ui->lineEdit->setText(name_of_document);
+                                  ui->lineEdit_2->setText(image);
+                                  ui->lineEdit_3->setText(description);
+
+
+
+                               }
+
+                               }
+           }
+           db.close();
+    }
+
+
 }
 
 
@@ -86,6 +166,7 @@ void Dialog_update_data::on_tableView_clicked(const QModelIndex &index)
 
 void Dialog_update_data::on_pushButton_2_clicked()
 {
+    if(type_of_data == "vistitors"){
     QSqlDatabase db = QSqlDatabase::database();
       db.setHostName("127.0.0.1");
       db.setUserName("postgres");
@@ -113,5 +194,123 @@ void Dialog_update_data::on_pushButton_2_clicked()
                      //close data base
                      db.close();
       }
+    }
+    if(type_of_data == "documents"){
+        QSqlDatabase db = QSqlDatabase::database();
+          db.setHostName("127.0.0.1");
+          db.setUserName("postgres");
+          db.setPassword("elkin");
+          db.setDatabaseName("postgres");
+          if(db.open()){
+              QString name = ui->lineEdit->text();
+              QString group_user = ui->lineEdit_2->text();
+
+
+              qDebug("open");
+                          query = new QSqlQuery(db);
+                        //write down sql request
+                        query->prepare("UPDATE documents  SET name_file=:0 , group_user=:1  WHERE id=:2 ");
+
+                        //bind Values
+                         query->bindValue(0,name);
+                         query->bindValue(1,group_user);
+                         query->bindValue(2,row);
+
+                         if(query->exec()){
+                                         QMessageBox::information(this,"Информация!","Информация была успешно обновлена.");
+                                     }
+                         //close data base
+                         db.close();
+          }
+    }
+    if(type_of_data == "books"){
+        QSqlDatabase db = QSqlDatabase::database();
+          db.setHostName("127.0.0.1");
+          db.setUserName("postgres");
+          db.setPassword("elkin");
+          db.setDatabaseName("postgres");
+          if(db.open()){
+              QString name = ui->lineEdit->text();
+              QString image = ui->lineEdit_2->text();
+              QString description = ui->lineEdit_3->text();
+
+              qDebug("open");
+                          query = new QSqlQuery(db);
+                        //write down sql request
+                        query->prepare("UPDATE books  SET name=:0 , image=:1 , description=:2  WHERE id=:3 ");
+
+                        //bind Values
+                         query->bindValue(0,name);
+                         query->bindValue(1,image);
+                         query->bindValue(2,description);
+                         query->bindValue(3,row);
+
+                         if(query->exec()){
+                                         QMessageBox::information(this,"Информация!","Информация была успешно обновлена.");
+                                     }
+                         //close data base
+                         db.close();
+          }
+    }
+}
+
+
+void Dialog_update_data::on_pushButton_3_clicked()
+{
+    type_of_data ="documents";
+    QSqlDatabase conn = QSqlDatabase::database();
+        conn.setHostName("127.0.0.1");
+        conn.setUserName("postgres");
+        conn.setPassword("elkin");
+        conn.setDatabaseName("postgres");
+        if(conn.open()){
+
+
+            // get model
+                modal = new QSqlTableModel(this,conn);
+                QSqlQuery* query = new QSqlQuery(conn);
+                QSqlQueryModel * modal = new QSqlQueryModel();
+
+                query->prepare("select * from documents order by id asc ");
+
+                query->exec();
+                modal->setQuery(*query);
+                ui->tableView->setModel(modal);
+
+             }
+        else{
+            qDebug()<<"unconnected";
+        }
+        conn.close();
+}
+
+
+void Dialog_update_data::on_pushButton_4_clicked()
+{
+    type_of_data = "books";
+    QSqlDatabase conn = QSqlDatabase::database();
+        conn.setHostName("127.0.0.1");
+        conn.setUserName("postgres");
+        conn.setPassword("elkin");
+        conn.setDatabaseName("postgres");
+        if(conn.open()){
+
+
+            // get model
+                modal = new QSqlTableModel(this,conn);
+                QSqlQuery* query = new QSqlQuery(conn);
+                QSqlQueryModel * modal = new QSqlQueryModel();
+
+                query->prepare("select * from books order by id asc ");
+
+                query->exec();
+                modal->setQuery(*query);
+                ui->tableView->setModel(modal);
+
+             }
+        else{
+            qDebug()<<"unconnected";
+        }
+        conn.close();
 }
 
--- admin_archive_lmk/dialog_update_data.h
+++ admin_archive_lmk/dialog_update_data.h
@@ -30,6 +30,10 @@ private slots:
 
     void on_pushButton_2_clicked();
 
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
 private:
     Ui::Dialog_update_data *ui;
     QSqlQuery *query ;
